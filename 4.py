1743ade318e3fab543120d309493388a52ae0707
Connor Radley <connor.radley@hotmail.co.uk>
2022-05-03 12:56:15 +0100

    days 2 - 7

---
+++ 4.py
@@ -0,0 +1,41 @@
+from utils import display_solutions
+
+
+def is_winning(board, combination):
+    for j in range(5):
+        if set(board[j * 5 if j else 0:(j+ 1) *5]).issubset(set(combination)):
+            return [int(x) for x in board[j * 5 if j else 0:(j+ 1) *5]]
+        elif set([board[j], board[j + 5], board[j + 10], board[j + 15], board[j + 20]]).issubset(set(combination)):
+            return [int(x) for x in [board[j], board[j + 5], board[j + 10], board[j + 15], board[j + 20]]]
+
+def part_one(boards):
+    for i in range(len(draws)):
+        if i < 5: 
+            continue
+        for b in boards:
+            winning_combination = is_winning(b, draws[0:i+1])
+            if winning_combination:
+                return int(draws[i]) * sum([int(y) for y in b if y not in draws[0:i+1]])
+
+def part_two(boards):
+    round_2_winners = []
+    for i in range(len(draws)):
+        if i < 5: 
+            continue
+        for b in boards:
+            if b in round_2_winners:
+                continue
+            winning_combination = is_winning(b, draws[0:i+1])
+            if winning_combination:
+                if len(round_2_winners) == len(boards) - 1:
+                    return int(draws[i]) * sum([int(y) for y in b if y not in draws[0:i+1]])
+                round_2_winners.append(b)
+
+if __name__ == "__main__":
+    with open('./inputs/4.txt', 'r') as f:
+        lines = f.read()
+    draws = lines.split('\n')[0].split(',')
+    boards_unparsed = lines.split('\n\n')[1:]
+    boards = [b.split() for b in boards_unparsed]
+
+    display_solutions(part_one(boards), part_two(boards))
--- utils.py
+++ utils.py
@@ -4,5 +4,9 @@ def file_as_int_list(filepath: str) -> List[int]:
     with open(filepath, 'r') as f:
         return [int(line) for line in f.readlines()]
 
+def file_as_list(filepath: str) -> List[int]:
+    with open(filepath, 'r') as f:
+        return f.read().split('\n')
+
 def display_solutions(solution_one: int, solution_two: int) -> None:
     print(f'Solution 1: {solution_one}\nSolution 2: {solution_two}')
