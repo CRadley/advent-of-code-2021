47356dd35b4e8a36ce9567eff8e805cbe4847e38
Connor Radley <connor.radley@hotmail.co.uk>
2022-05-15 10:14:14 +0100

    Refactor day3

--- aoc/day3/day3.py
+++ aoc/day3/day3.py
@@ -9,48 +9,22 @@ def get_counts(bins: List[int]) -> int:
     return counts
 
 def part_one(bins: List[int]) -> int:
-    counts = [[0,0] for _ in range(len(str(bins[0])))]
-    for bin_ in bins:
-        for (i, bit) in enumerate(str(bin_)):
-            counts[i][int(bit)] += 1
-    g = ''
-    e = ''
-    for count in counts:
-        if count[0] < count[1]:
-            g += str(1)
-            e += str(0)
-        else:
-            g += str(0)
-            e += str(1)
+    counts = get_counts(bins)
+    g = "".join(map(str, [int(count[0] < count[1]) for count in counts]))
+    e = "".join(map(str, [int(count[0] > count[1]) for count in counts]))
     return int(g, 2) * int(e,2)
 
 def part_two(bins: List[int]) -> int:
     counts = get_counts(bins)
-    output_bins_o = bins[:]
-    output_bins_c = bins[:]
-    for bin_ in bins:
-        for (i, bit) in enumerate(str(bin_)):
-            counts[i][int(bit)] += 1
+    output_bins_o = [x for x in bins]
+    output_bins_c = [x for x in bins]
     for i, _ in enumerate(counts):
         o_counts = get_counts(output_bins_o)
         c_counts = get_counts(output_bins_c)
-        x = '1' if o_counts[i][1] >= o_counts[i][0] else '0'
-        y = '1' if c_counts[i][1] < c_counts[i][0] else '0'
+        most_common = str(int(o_counts[i][1] >= o_counts[i][0]))
+        least_common = str(int(c_counts[i][1] < c_counts[i][0]))
         if len(output_bins_o) > 1:
-            output_bins_o = [bin_ for bin_ in output_bins_o if bin_[i] == x]
-
+            output_bins_o = [bin_ for bin_ in output_bins_o if bin_[i] == most_common]
         if len(output_bins_c) > 1:
-            output_bins_c = [bin_ for bin_ in output_bins_c if bin_[i] == y]
+            output_bins_c = [bin_ for bin_ in output_bins_c if bin_[i] == least_common]
     return int(output_bins_o[0], 2) * int(output_bins_c[0],2)
-
-def main():
-    inputs = file_as_list('./inputs/3.txt')
-    solution_one = part_one(inputs)
-    solution_two = part_two(inputs)
-    display_solutions(solution_one, solution_two)
-
-if __name__ == '__main__':
-    main()
-
-#Solution 1: 3320834
-#Solution 2: 4481199
